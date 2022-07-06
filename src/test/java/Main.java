2e2dffec9d09536e64e53dd7101b6c7dcc05b3a3
seirena <seirena1337@gmail.com>
2022-07-06 19:26:57 +0300

    remove main.java

--- src/test/java/Main.java
+++
@@ -1,7 +0,0 @@
-import org.junit.runner.JUnitCore;
-
-public class Main {
-    public static void main(String[] args) {
-        JUnitCore.runClasses(TestClass.class,ParameterizedTests.class);
-    }
-}
